3bea68bce7acac61aadbb7276b888f93f5cbb956
jbthelegend <jbthelegend15@gmail.com>
2024-04-02 19:52:41 -0500

    k

--- dashboards/src/components/NavBar.js
+++ dashboards/src/components/NavBar.js
@@ -6,60 +6,24 @@ export default function Navbar(props) {
         <>
             <header className="bg-blue-500 p-4">
                 <nav className="flex justify-left space-x-4">
-                    <Link to="/">
                         <img
                             src={"logo.png"}
                             alt="Charity Clarity Logo"
                             className="h-12"
                         />
-                    </Link>
-                    <Link
-                        to="/"
                         className={`text-white px-4 py-2 rounded-lg font-bold text-xl `}
-                    >
+                    
                         Charity Clarity
-                    </Link>
-                    <Link
-                        to="/donate"
+                    <div>
                         className={`${
                             props.active === "donate"
                                 ? "bg-white text-blue-500"
                                 : "text-white"
                         } px-4 py-2 rounded-full font-bold text-xl hover:bg-white hover:text-blue-500`}
-                    >
+                    
                         Donate
-                    </Link>
-                    <Link
-                        to="/contributions"
-                        className={` ${
-                            props.active === "contributions"
-                                ? "bg-white text-blue-500"
-                                : "text-white"
-                        } px-4 py-2 rounded-full font-bold text-xl hover:bg-white hover:text-blue-500`}
-                    >
-                        Contributions
-                    </Link>
-                    <Link
-                        to="/impact"
-                        className={` ${
-                            props.active === "impact"
-                                ? "bg-white text-blue-500"
-                                : "text-white"
-                        } px-4 py-2 rounded-full font-bold text-xl hover:bg-white hover:text-blue-500`}
-                    >
-                        Impact
-                    </Link>
-                    <div style={{ width: "600px" }}></div> {/* Spacer */}
-                    <Link
-                        to="/login"
-                        className={`justify-right ${
-                            props.active === "login"
-                                ? "bg-white text-blue-500"
-                                : "text-white"
-                        } px-4 py-2 rounded-full font-bold text-xl hover:bg-white hover:text-blue-500`}
-                    >
-                        Login
-                    </Link>
+                    </div>
+                    
                 </nav>
             </header>
         </>
