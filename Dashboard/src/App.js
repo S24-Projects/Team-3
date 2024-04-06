1796b86e8adb5fe4a124c3753cf821106b52213c
jeremiahb <jbthelegend15@gmail.com>
2024-04-06 18:23:03 -0500

    added a scrollup button, and random content in middle

--- Dashboard/src/App.js
+++ Dashboard/src/App.js
@@ -1,7 +1,8 @@
+import React from "react";
 import './App.css';
 import NavBar from "./components/NavBar";
 import Table from "./components/Table";
-import React from "react";
+import ToTopButton from "./components/ToTopButton.js";
 import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
@@ -15,25 +16,25 @@ function App() {
       </div>
       <ToTopButton />
 
-                {/* Hero Section */}
-                <div className="bg-blue-500 text-white py-20 px-4">
-                    <div className="max-w-4xl mx-auto text-center">
-                        <h1 className="text-4xl font-bold mb-4">
-                            Using Blockchain for Transparency
-                        </h1>
-                        <p className="text-lg mb-8">
-                            Support our cause by donating with cryptocurrency on
-                            the Solana blockchain. Ensure transparency with
-                            every donation.
-                        </p>
-                        <button
-                            //onClick={() => navigate("/donate")}
-                            className="bg-white text-blue-500 py-2 px-6 rounded-full font-semibold hover:bg-blue-700 hover:text-white transition duration-300"
-                        >
-                            Donate Now
-                        </button>
-                    </div>
-                </div>
+      {/* Hero Section */}
+      <div className="bg-blue-500 text-white py-20 px-4">
+          <div className="max-w-4xl mx-auto text-center">
+              <h1 className="text-4xl font-bold mb-4">
+                  Using Blockchain for Transparency
+              </h1>
+              <p className="text-lg mb-8">
+                  Support our cause by donating with cryptocurrency on
+                  the Solana blockchain. Ensure transparency with
+                  every donation.
+              </p>
+              <button
+                  //onClick={() => navigate("/donate")}
+                  className="bg-white text-blue-500 py-2 px-6 rounded-full font-semibold hover:bg-blue-700 hover:text-white transition duration-300"
+              >
+                  Donate Now
+              </button>
+          </div>
+      </div>
       <Table />
     </Router>
   );
