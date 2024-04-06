89d2119a813faa6627b8114c1476d7bca227a187
jeremiahb <jbthelegend15@gmail.com>
2024-04-06 18:28:44 -0500

    changed logo and html tag name

--- Dashboard/src/App.js
+++ Dashboard/src/App.js
@@ -9,33 +9,32 @@ function App() {
   return (
     <Router>
       <NavBar />
+      <ToTopButton />
       <div className="left-container"> {/* Container to center the button */}
         <button className="custom-button text-white font-bold py-2 px-4 rounded-full">
           Invest
         </button>
       </div>
-      <ToTopButton />
-
+      
+      <Table />
       {/* Hero Section */}
-      <div className="bg-blue-500 text-white py-20 px-4">
+      <div className="fintech-color text-white py-20 px-4">
           <div className="max-w-4xl mx-auto text-center">
               <h1 className="text-4xl font-bold mb-4">
-                  Using Blockchain for Transparency
+                  Past Performance
               </h1>
               <p className="text-lg mb-8">
-                  Support our cause by donating with cryptocurrency on
-                  the Solana blockchain. Ensure transparency with
-                  every donation.
+                  Try our Robo Advisor which analyzes market stock data and factors in your individual risk preference. 
               </p>
               <button
                   //onClick={() => navigate("/donate")}
                   className="bg-white text-blue-500 py-2 px-6 rounded-full font-semibold hover:bg-blue-700 hover:text-white transition duration-300"
               >
-                  Donate Now
+                  Take the risk questionaire. 
               </button>
           </div>
       </div>
-      <Table />
+      
     </Router>
   );
 }
